99657672674b255923294c8e3b8a3ce9af8698a6
Serdar Tumgoren <tumgorenz@washpost.com>
2012-01-14 04:46:42 -0500

    fleshed out Contact model and its admin

--- sacredheartcs/apps/contacts/admin.py
+++ sacredheartcs/apps/contacts/admin.py
@@ -3,14 +3,51 @@ from django.contrib import admin
 from models import Contact, ContactList, ListMembership
 
 
+class ListMembershipAdmin(admin.ModelAdmin):
+    list_filter = ('contact_list',)
+
 class ListMembershipInline(admin.TabularInline):
     model = ListMembership
+    extra = 0
 
 class ContactAdmin(admin.ModelAdmin):
     inlines = [
         ListMembershipInline,
     ]
+    list_display = ('email_address','company_name', 'purged', 'purge_candidate','opened_in_six_months') 
+    list_filter = ('opened_in_six_months', 'purged', 'purge_candidate')
+
+    #field_set  status (purged, purge_candidate, etc.), info (name, address, etc.), meta (date_added, added_by)
+    fieldsets = (
+        (None, {
+            'fields':(('email_address', 'email_type'),)
+        }),
+        ('Status', {
+            'classes':('collapse',),
+            'fields': (('purged', 'purge_candidate', 'opened_in_six_months'),),
+        }),
+        ('Contact Info', {
+            'classes':('collapse',),
+            'fields': (
+                    ('first_name', 'middle_name', 'last_name'),
+                    ('company_name', 'job_title'),
+                    ('work_phone', 'home_phone'),
+                    'address_line_1',
+                    'address_line_2',
+                    'address_line_3',
+                    'city',
+                    ('us_state_or_ca_province', 'other_state_or_province'),
+                    ('zip_or_postal_code', 'sub_zip_or_postal_code'),
+                    'country',
+            ),
+        }),
+        ('Meta', {
+            'classes':('collapse',),
+            'fields':('date_added', 'added_by'),
+        }),
+
+    )
 
 admin.site.register(Contact, ContactAdmin)
 admin.site.register(ContactList)
-admin.site.register(ListMembership)
+admin.site.register(ListMembership, ListMembershipAdmin)
--- sacredheartcs/apps/contacts/models.py
+++ sacredheartcs/apps/contacts/models.py
@@ -3,14 +3,14 @@ from django.template.defaultfilters import slugify
 
 
 class Contact(models.Model):
-    email_address = models.CharField(max_length=250)
+    email_address = models.CharField(max_length=250, unique=True)
     email_type = models.CharField(max_length=250)
     first_name = models.CharField(max_length=70, blank=True)
     middle_name = models.CharField(max_length=70, blank=True)
-    last_name = models.CharField(max_length=70, blank=True)
+    last_name = models.CharField(max_length=70, blank=True, db_index=True)
     job_title = models.CharField(max_length=200, blank=True)
-    company_name = models.CharField(max_length=250, blank=True)
-    date_added = models.DateTimeField(blank=True, null=True)
+    company_name = models.CharField(max_length=250, blank=True, db_index=True)
+    date_added = models.DateTimeField(blank=True, null=True, db_index=True)
     added_by = models.CharField(max_length=250, blank=True)
     work_phone = models.CharField(max_length=12, blank=True)
     home_phone = models.CharField(max_length=12, blank=True)
@@ -18,7 +18,7 @@ class Contact(models.Model):
     address_line_2 = models.CharField(max_length=200, blank=True)
     address_line_3 = models.CharField(max_length=200, blank=True)
     city = models.CharField(max_length=200, blank=True)
-    us_state_or_ca_province = models.CharField(max_length=200, blank=True)
+    us_state_or_ca_province = models.CharField(max_length=200, blank=True, db_index=True)
     other_state_or_province = models.CharField(max_length=200, blank=True)
     country = models.CharField(max_length=200, blank=True)
     zip_or_postal_code = models.CharField(max_length=10, blank=True)
@@ -26,12 +26,14 @@ class Contact(models.Model):
     notes = models.TextField(blank=True)
 
     # STATUS FLAGS & CALCULATED FIELDS
-    opened_in_six_months = models.IntegerField(blank=True, null=True)
-    purged = models.BooleanField(default=False)
-    purge_candidate = models.BooleanField(default=False)
+    opened_in_six_months = models.IntegerField(blank=True, null=True, db_index=True,
+                                               help_text='Number of times this contact opened email in last 6 months. Updated daily.')
+    purged = models.BooleanField(default=False, db_index=True)
+    purge_candidate = models.BooleanField(default=False, db_index=True)
+
 
     def __unicode__(self):
-        return self.email
+        return self.email_address
 
 class ContactList(models.Model):
     """
@@ -45,6 +47,10 @@ class ContactList(models.Model):
     slug = models.SlugField(max_length=250)
     contacts = models.ManyToManyField(Contact, through='ListMembership', help_text='Constant Contacts lists this person is assigned to')
 
+    class Meta:
+        unique_together = ('name',)
+        ordering = ['name']
+
     def __unicode__(self):
         return self.name
 
@@ -57,60 +63,12 @@ class ListMembership(models.Model):
     contact = models.ForeignKey(Contact)
     contact_list = models.ForeignKey(ContactList)
 
+    class Meta:
+        unique_together = ('contact', 'contact_list')
+        ordering = ['contact_list']
+
+    def __unicode__(self):
+        return '%s (%s)' % (self.contact, self.contact_list)
+
 #class EmailCampaign (an outreach email sent to potential volunteers/donors)
 #class EmailAction (response to outreach emails)
-
-
-"""
-Email Address,
-Email Type,
-List: General Interest,
-List: Donors,
-List: Volunteers,
-List: staff,
-List: Schools,
-List: Service/Social Orgs,
-List: 2010 holiday volunteers,
-List: LMV Master List,
-List: silicon valley volunteer 6-25/26,
-List: Pack-a-Back volunteers 2010-11,i
-List: 2011 Holiday volunteers,
-List: master,
-List: Microsoft Store 12-29-11,
-List: Student Conference 12-29-11,
-List: Saturday Volunteers 1/7/12,
-Date Added,
-Added By,
-First Name,
-Middle Name,
-Last Name,
-Job Title,
-Company Name,
-Work Phone,
-Home Phone,
-Address Line 1,
-Address Line 2,
-Address Line 3,
-City,
-US State/CA Province,
-Other State/Province,
-Country,
-Zip/Postal Code,
-Sub Zip/Postal Code,
-Notes,
-Custom field 1,
-Custom field 2,
-Custom field 3,
-Custom field 4,
-Custom field 5,
-Custom field 6,
-Custom field 7,
-Custom field 8,
-Custom field 9,
-Custom field 10,
-Custom field 11,
-Custom field 12,
-Custom field 13,
-Custom field 14,
-Custom field 15
-"""
