5ad1ef7f933bed7a9efb202d26a098f23e96a57e
Serdar Tumgoren <tumgorenz@washpost.com>
2012-02-09 23:56:26 -0500

    added SQL for generating lists of openers

---
+++ sacredheartcs/apps/contacts/sql/get_openers_since_X_date.sql
@@ -0,0 +1,19 @@
+﻿select distinct(action_date) from contacts_emailaction order by 1 desc;
+
+/* get distinct email addresses for all contacts who opened a message since X date */
+select distinct(c.email_address)
+from contacts_contact c 
+join contacts_emailaction e on e.contact_id = c.id
+where e.action_date > '2011-06-01'
+order by c.email_address
+;
+
+/* count # of opens, by email address, since X date */
+select c.email_address, count(e.action_date) "number_opened"
+from contacts_contact c 
+join contacts_emailaction e on e.contact_id = c.id
+where e.action_date > '2011-06-01'
+group by c.email_address
+--order by c.email_address
+order by 2 desc, 1
+;
